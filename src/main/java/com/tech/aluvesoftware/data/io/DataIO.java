ca29596b5953a8dbd33979d76094bbe720119679
Tebogo Lefatola <tebogo@aluvesqe.co.za>
2023-09-12 13:36:51 +0200

    --

--- src/main/java/com/tech/aluvesoftware/data/io/DataIO.java
+++ src/main/java/com/tech/aluvesoftware/data/io/DataIO.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import org.apache.logging.log4j.Logger;
 
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -21,8 +20,19 @@ public class DataIO {
     private static final Logger logger = getLogger(lookup().lookupClass());
     private static final Gson gson = new Gson();
 
-    private DataIO(){}
+    private DataIO() {
+    }
 
+
+    /**
+     * Converts a JSON string into a Java object of the specified class using the Jackson ObjectMapper.
+     *
+     * @param jsonString  The JSON string to be converted.
+     * @param chosenClass The class type into which the JSON should be converted.
+     * @param <T>         The type of the object to convert to.
+     * @return An object of the specified class representing the JSON data.
+     * @throws RuntimeException if there is an error during the conversion.
+     */
     public static <T> T convertJsonToObject(String jsonString, Class<T> chosenClass) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -32,11 +42,23 @@ public class DataIO {
         }
     }
 
-    public static String simpleJsonRequest(Object o){
+    /**
+     * Converts a Java object into a JSON string using the Gson library.
+     *
+     * @param o The Java object to be converted to JSON.
+     * @return A JSON string representation of the Java object.
+     */
+    public static String simpleJsonRequest(Object o) {
         return gson.toJson(o);
     }
 
-    public static String marshallEnvelopeObject(Object ob){
+    /**
+     * Marshals a Java object into an XML string using JAXB.
+     *
+     * @param ob The Java object to be marshaled into XML.
+     * @return An XML string representation of the Java object, or null if an error occurs.
+     */
+    public static String marshallEnvelopeObject(Object ob) {
         StringWriter sw = new StringWriter();
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(ob.getClass());
@@ -52,13 +74,21 @@ public class DataIO {
         return sw.toString();
     }
 
-    public static <T> T unMarshalingEnvelopeObject(String xml, Class<T> chosenClass){
+    /**
+     * Unmarshals an XML string into an object of the specified class using JAXB.
+     *
+     * @param xml         The XML string to unmarshal.
+     * @param chosenClass The class type into which the XML should be unmarshaled.
+     * @param <T>         The type of the object to unmarshal.
+     * @return An object of the specified class unmarshaled from the XML string, or null if an error occurs.
+     */
+    public static <T> T unmarshalEnvelopeObject(String xml, Class<T> chosenClass) {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(chosenClass);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             StringReader reader = new StringReader(xml);
             return chosenClass.cast(jaxbUnmarshaller.unmarshal(reader));
-        } catch (JAXBException jaxbException){
+        } catch (JAXBException jaxbException) {
             logger.info(String.format("%s%s", ERROR_PREFIX, jaxbException.getMessage()));
         }
         return null;
